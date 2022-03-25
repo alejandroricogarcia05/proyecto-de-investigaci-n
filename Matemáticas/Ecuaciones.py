221f103f25bd4abf584ce27057666429ff015f1d
AlexanderMayklMychlo <alexander.mychlo@educa.madrid.org>
2022-03-25 20:19:57 +0100

    Añadido los ejemplos de Inequaciones

--- Matemáticas/Ecuaciones.py
+++ Matemáticas/Ecuaciones.py
@@ -2,7 +2,7 @@ from sympy import *
 
 x = symbols('x') #Símbolo que queremos averiguar
 
-ecuacion = Eq((2*x - 4),0)
+ecuacion = Eq((2*x - 4),0) # 2*x - 4 = 0
 
-print(solve(ecuacion)) # 2*x - 4 = 0
+print(solve(ecuacion)) # [2]
 
